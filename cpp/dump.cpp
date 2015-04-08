159b5d1ff0e4fdee7e29bb9895027d02ff4f7755
hovy <hovy@informatik.uni-hamburg>
2015-04-08 15:06:14 +0200

    Commandline arguments for bounds for dump.

--- cpp/dump.cpp
+++ cpp/dump.cpp
@@ -5,37 +5,47 @@
  *      Author: hovy
  */
 
-#include <iostream>
+#include <unistd.h>    /* for getopt */
+#include <algorithm>   /* for max, min */
+#include <iostream>    /* for cout */
 #include "Serializer.h"
 #include "shared.h"
 
-void dumpInfo(const DataFieldInfo info)
+
+void dumpInfo(const DataFieldInfo info, Bounds iBounds, Bounds jBounds, Bounds kBounds, Bounds lBounds)
 {
-	std::cout << "iSize: " << info.iSize() << std::endl;
-	std::cout << "jSize: " << info.jSize() << std::endl;
-	std::cout << "kSize: " << info.kSize() << std::endl;
-	std::cout << "lSize: " << info.lSize() << std::endl;
+	iBounds.upper = std::min(iBounds.upper, info.iSize() - 1);
+	jBounds.upper = std::min(jBounds.upper, info.jSize() - 1);
+	kBounds.upper = std::min(kBounds.upper, info.kSize() - 1);
+	lBounds.upper = std::min(lBounds.upper, info.lSize() - 1);
+
+	std::cout << "iSize: " << info.iSize() << " " << iBounds << std::endl;
+	std::cout << "jSize: " << info.jSize() << " " << jBounds << std::endl;
+	std::cout << "kSize: " << info.kSize() << " " << kBounds << std::endl;
+	std::cout << "lSize: " << info.lSize() << " " << lBounds << std::endl;
 }
 
-void dumpData(const double* data, int iSize, int jSize, int kSize, int lSize)
+void dumpData(const double* data, int iSize, const Bounds iBounds, int jSize, const Bounds jBounds,
+								  int kSize, const Bounds kBounds, int lSize, const Bounds lBounds)
 {
 	bool equal = true;
 
-	for (int i = 0; i < iSize; ++i)
+	for (int i = std::max(0, iBounds.lower); i <= std::min(iSize - 1, iBounds.upper); ++i)
 	{
-		for (int j = 0; j < jSize; ++j)
+		for (int j = std::max(0, jBounds.lower); j <= std::min(jSize - 1, jBounds.upper); ++j)
 		{
 			if (kSize > 1) std::cout << "[ ";
-			for (int k = 0; k < kSize; ++k)
+			for (int k = std::max(0, kBounds.lower); k <= std::min(kSize - 1, kBounds.upper); ++k)
 			{
 				if (lSize > 1) std::cout << "( ";
-				for (int l = 0; l < lSize; ++l)
+				for (int l = std::max(0, lBounds.lower); l <= std::min(lSize - 1, lBounds.upper); ++l)
 				{
 					int index = i*jSize*kSize*lSize + j*kSize*lSize + k*lSize + l;
 					std::cout << data[index];
 					if (l < lSize - 1) std::cout << ", ";
 				}
 				if (lSize > 1) std::cout << " )";
+				if (k < kSize - 1) std::cout << ", ";
 			}
 			if (kSize > 1) std::cout << " ]";
 		}
@@ -43,25 +53,56 @@ void dumpData(const double* data, int iSize, int jSize, int kSize, int lSize)
 	}
 }
 
-void dump(const DataFieldInfo info, const double* data)
+void dump(const std::string directory, const std::string basename,
+	      const std::string field, const std::string savepoint,
+		  Bounds iBounds, Bounds jBounds, Bounds kBounds, Bounds lBounds)
 {
-	dumpInfo(info);
-	std::cout << std::endl;
-	dumpData(data, info.iSize(), info.jSize(), info.kSize(), info.lSize());
-}
-
-int main(int argc, char* argv[])
-{
-	const std::string directory = argv[1];
-	const std::string basename = argv[2];
-	const std::string field = argv[3];
-	const std::string savepoint = argv[4];
-
 	double* data;
 	DataFieldInfo info;
 
 	readFile(directory, basename, field, savepoint, info, data);
-	dump(info, data);
 
-	return 0;
+	dumpInfo(info, iBounds, jBounds, kBounds, lBounds);
+	std::cout << std::endl;
+	dumpData(data, info.iSize(), iBounds, info.jSize(), jBounds, info.kSize(), kBounds, info.lSize(), lBounds);
+}
+
+int main (int argc, char **argv) {
+    int opt;
+    std::string i = ":";
+    std::string j = ":";
+    std::string k = ":";
+    std::string l = ":";
+    while ( (opt = getopt(argc, argv, "i:j:k:l:")) != -1) {
+        switch (opt)
+        {
+        case 'i':
+			i = optarg;
+            break;
+        case 'j':
+			j = optarg;
+            break;
+        case 'k':
+			k = optarg;
+            break;
+        case 'l':
+			l = optarg;
+            break;
+        }
+    }
+
+    //TODO Nur die Datei als Parameter übergeben und directory, basename und field selbst herausfinden
+    const std::string directory = argv[optind++];
+	const std::string basename = argv[optind++];
+	const std::string field = argv[optind++];
+	const std::string savepoint = argv[optind++];
+
+	Bounds iBounds = string2bounds(i);
+	Bounds jBounds = string2bounds(j);
+	Bounds kBounds = string2bounds(k);
+	Bounds lBounds = string2bounds(l);
+
+	dump(directory, basename, field, savepoint, iBounds, jBounds, kBounds, lBounds);
+
+    exit (0);
 }
--- cpp/shared.h
+++ cpp/shared.h
@@ -1,5 +1,58 @@
+#include <cstdlib>
+#include <algorithm>
+#include <limits>
+
 #ifndef NEW_SERIALIZER_COMPARE_SHARED
 #define NEW_SERIALIZER_COMPARE_SHARED
+
+struct Bounds {
+	int lower, upper;
+};
+
+std::ostream& operator << (std::ostream &o, const Bounds &bounds)
+{
+	o << "(" << bounds.lower << ", " << bounds.upper << ")";
+	return o;
+}
+
+Bounds string2bounds(std::string boundString)
+{
+	Bounds bounds = { 0, std::numeric_limits<int>::max() };
+
+	if (boundString[0] != ':')
+	{
+		const char *s = boundString.c_str();
+		char *t;
+		bounds.lower = strtol(s, &t, 10);
+		boundString = std::string(t);
+		if (boundString.length() == 0)
+		{
+			bounds.upper = bounds.lower;
+		}
+		else
+		{
+			boundString = boundString.substr(1);
+		}
+	}
+	else
+	{
+		boundString = boundString.substr(1);
+	}
+
+	if (boundString.length() > 0)
+	{
+		const char *s = boundString.c_str();
+		char *t;
+		bounds.upper = strtol(s, &t, 10);
+	}
+
+	if (bounds.upper < bounds.lower)
+	{
+		std::swap(bounds.upper, bounds.lower);
+	}
+
+	return bounds;
+}
 
 void readFile(const std::string directory, const std::string basename, const std::string savepointName, std::string field, DataFieldInfo& info, double*& data)
 {
